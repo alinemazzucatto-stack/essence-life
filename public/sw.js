d717837efeaa7e9994791d68f05898da978d5e1b
Essence Life Publisher <essence-life@local.invalid>
2026-08-23 15:07:34 -0300

    style: center module content and box navigation tabs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'essence-life-shell-v17-rose';
+const CACHE = 'essence-life-shell-v18-rose';
 self.addEventListener('install', event => { event.waitUntil(self.skipWaiting()) });
 self.addEventListener('activate', event => { event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])) });
 self.addEventListener('fetch', event => {
